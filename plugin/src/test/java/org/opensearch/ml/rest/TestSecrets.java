e280778b323a1f104dbc9851109915b455fda73c
Ryan Bogan <rbogan@amazon.com>
2023-07-11 05:53:22 +0000

    Add dummy test
    Signed-off-by: Ryan Bogan <rbogan@amazon.com>

---
+++ plugin/src/test/java/org/opensearch/ml/rest/TestSecrets.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.rest;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestSecrets {
+    
+    public void testSecrets() {
+        assertEquals(System.getenv("TEST_SECRET"), "Testing1234");
+    }
+
+}
