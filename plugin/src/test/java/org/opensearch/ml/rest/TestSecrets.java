fb0dc815ca7316be488321a5eaf8cb64d1aecb06
Ryan Bogan <rbogan@amazon.com>
2023-07-11 06:15:28 +0000

    Change test
    Signed-off-by: Ryan Bogan <rbogan@amazon.com>

--- plugin/src/test/java/org/opensearch/ml/rest/TestSecrets.java
+++ plugin/src/test/java/org/opensearch/ml/rest/TestSecrets.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertEquals;
 public class TestSecrets {
 
     public void testSecrets() {
-        assertEquals(System.getenv("TEST_SECRET"), "Testing1234");
+        assertEquals(System.getenv("TEST_SECRET"), "Testing1235");
     }
 
 }
